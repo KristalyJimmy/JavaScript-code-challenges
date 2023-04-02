38584decd6657b53737bf9b809256d2ee0132a19
KristalyJimmy <kristalyjimmy@yahoo.com>
2023-04-02 20:47:57 +0200

    tenth task completed

--- index.js
+++ index.js
@@ -56,3 +56,15 @@ const numberDividedByTen = (number) => {
 }
 console.log(numberDividedByTen(2));
 //10. Return the number of vowels in a string
+const loremIpsum = 'Lorem ipsum dolor sit amet';
+const countVowels = (str) => {
+    const vowels = 'aeiouAEIOU';
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        if(vowels.includes(str[i].toLowerCase())){
+            count++;
+        }
+    }
+    return count;
+}
+console.log('Number of vowels in the text: '+countVowels(loremIpsum));
