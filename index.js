413c73616e666bca5a17a7f8456ad9fa6f2cb567
KristalyJimmy <kristalyjimmy@yahoo.com>
2023-04-02 17:28:08 +0200

    third task completed

--- index.js
+++ index.js
@@ -7,12 +7,21 @@ console.log(evenNumbers(numbers));
 
 //2. Print a table containing multiplication tables
 const arrayOfNumbers = [1,2,3,4,5,6,7,8,9,10];
-const multiplication = arrayOfNumbers.map(num => num *2);
+const multiplication = arrayOfNumbers.map(num => num * 2);
 console.log(multiplication);
 
+//3. Create a length converter function from kilometers to miles
+const kmToMileConverter = (km) => {
+    const miles = km * 0.621371;
+    return `${km} kilometers is equal to ${miles} miles.`;
+  }
+  console.log(kmToMileConverter(2));
 
-
-//3. Create a length converter function
+  const celsiusToFahrenheit = (celsius) => {
+    const fahrenheit = celsius * 1.8 + 32;
+    return `${celsius} celsius is equal to ${fahrenheit} fahrenheit.`;
+  }
+  console.log(celsiusToFahrenheit(20));
 //4. Calculate the sum of numbers within an array
 //5. Create a function that reverses an array
 //6. Sort an array from lowest to highest
